65e0130aa6ec16856a37adb3ad37e37fd65f7889
Zach G <Zach.the.gecko@gmail.com>
2021-10-14 09:28:08 -0600

    added like-count number along with interactive toggle on/off switches

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
-const subcount = document.getElementById("number");
+const subcount = document.getElementById("subCountNumber");
+const likecount = document.getElementById("likeCountNumber");
 
-const channelId = "UCLPxXrK_Wkmlq301zOUJ-2g";
+const subcountcontainer = document.getElementById("subcountcontainer");
+const likecountcontainer = document.getElementById("likecountcontainer");
 
-//gamerboy 80 UCuAj31T2G04FZkCR-6carEA
+const channelId = "UCLPxXrK_Wkmlq301zOUJ-2g"; // 0rtt
+const videoId = window.prompt("Enter the video ID"); // Set video ID value here
+
+const apiKey = "AIzaSyD8Xb8SL4FLbnLAnzpNb4Z-LXAkAonLzGE";
 
 const timer = () => {
   setTimeout(() => {
     fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=statistics&id=" +
+        videoId +
+        "&key=" +
+        apiKey
+    )
+      .then((json) => json.json())
+      .then((res) => {
+        likecount.innerHTML = res.items[0].statistics.likeCount;
+      })
+      .catch(() => (likecount.innerHTML = "0"));
+
+    fetch(
       "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" +
         channelId +
-        "&key=AIzaSyD8Xb8SL4FLbnLAnzpNb4Z-LXAkAonLzGE"
+        "&key=" +
+        apiKey
     )
       .then((json) => json.json())
       .then((res) => {
         subcount.innerHTML = res.items[0].statistics.subscriberCount;
       })
-      .catch(() => (subcount.innerHTML = "undefined"));
+      .catch(() => (subcount.innerHTML = "0"));
     timer();
   }, 8000);
 };
 
 timer();
+
+subcountcontainer.addEventListener("click", () => {
+  console.log("click");
+  if (subcountcontainer.style.opacity == 0) {
+    subcountcontainer.style.opacity = 1;
+  } else {
+    subcountcontainer.style.opacity = 0;
+  }
+});
+
+likecountcontainer.addEventListener("click", () => {
+  if (likecountcontainer.style.opacity == 0) {
+    likecountcontainer.style.opacity = 1;
+  } else {
+    likecountcontainer.style.opacity = 0;
+  }
+});
